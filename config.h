6e7d5af23b1304d26b36f425f995f748c444dc47
Vivek Kalyan <hello@vivekkalyan.com>
2020-06-05 23:42:59 +0800

    Add swallow patch

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:style=Bold:size=10" };
@@ -30,11 +31,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class                        instance    title       tags mask   switchtotag     iscentered  isfloating  monitor */
-	{ "firefoxdeveloperedition",    NULL,       NULL,       1 << 0,     0,              0,          0,          -1 },
-	{ "feh",                        NULL,       NULL,       0,          0,              0,          1,          -1 },
-	{ "Nextcloud",                  NULL,       NULL,       1 << 8,     0,              0,          1,          -1 },
-	{ "Zotero",                     NULL,       NULL,       1 << 8,     0,              0,          0,          -1 },
+	/* class                        instance    title               tags mask   switchtotag     iscentered  isfloating  isterminal  noswallow   monitor */
+	{ "firefoxdeveloperedition",    NULL,       NULL,               1 << 0,     0,              0,          0,          0,          0,          -1 },
+	{ "feh",                        NULL,       NULL,               0,          0,              0,          1,          0,          0,          -1 },
+	{ "Nextcloud",                  NULL,       NULL,               1 << 8,     0,              0,          1,          0,          0,          -1 },
+	{ "Zotero",                     NULL,       NULL,               1 << 8,     0,              0,          0,          0,          0,          -1 },
+	{ "st",                         NULL,       NULL,               0,          0,              0,          0,          1,          -1,         -1 },
+	{ NULL,                         NULL,       "Event Tester",     0,          0,              0,          1,          0,           1,         -1 }, /* xev */
 };
 
 /* layout(s) */
